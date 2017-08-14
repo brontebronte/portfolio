6dde8a4b868e53d8e6f7edf039d985c4e4e942f9
Bronte <bronte.bronte.20@gmail.com>
2017-08-14 14:20:19 +0800

    Before deleting the default react framework, and re-pathing new ones

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+/////////////////////////////////////////////////
+//  Main React File (ties react to html file) //
+////////////////////////////////////////////////
+
+//////////////////
+//   Imports   //
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+////////////////////////////
+//   Renders onto html   //
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
