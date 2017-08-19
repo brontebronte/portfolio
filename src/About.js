b31fc8861aea4c925427e7458dc1f1167892f644
Bronte <bronte.bronte.20@gmail.com>
2017-08-20 03:13:06 +0800

    shadowing for svg done

--- src/About.js
+++ src/About.js
@@ -18,8 +18,10 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <h2 className="about__title">about</h2>
-        <img className="about__profile-pic" src={profilePic} alt="profile on milk carton" />
+        <div className="about__milkcarton">
+          <h2 className="about__milkcarton--title">about</h2>
+          <img className="about__milkcarton--profile-pic" src={profilePic} alt="profile on milk carton" />
+        </div>
         <p className="about__context">
         " I am like a sponge waiting to be thrown into the sea. A sponge which is wanting to remain afloat, but can't help soaking up the infinite sea of knowledge and challenges life has to offer. I am originally from a commercial background, but decided to switch industries as I wanted to be part of a product's creation process. "
         </p>
--- src/Gallery.js
+++ src/Gallery.js
@@ -19,8 +19,7 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="portfolio">
-        <h2 className="portfolio__gallery">gallery</h2>
-        <img className="portfolio__plate" src={plate} alt="plate" />
+        <h2 className="portfolio__title">gallery</h2>
         <img className="portfolio__fish" src={fish} alt="blue fish" />
       </div>
     );
